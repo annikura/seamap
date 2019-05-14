6abfb5229c493756f171fba7c698195a7798bd11
Ann <annikura@yandex.ru>
2019-05-14 21:08:38 +0300

    Add ErrorOr class

--- src/main/java/ErrorOr.java
+++ src/main/java/ErrorOr.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+public class ErrorOr<T> {
+    final private T obj;
+    final private String err;
+    final private boolean isErr;
 
-public class ErrorOr {
+    private ErrorOr(T o) {
+        obj = o;
+        err = "";
+        isErr = false;
+    }
+
+    private ErrorOr(String s, Void v) {
+        obj = null;
+        err = s;
+        isErr = true;
+    }
+
+    public static <T> ErrorOr<T> createObj(T obj) {
+        return new ErrorOr<>(obj);
+    }
+
+    public static <T> ErrorOr<T> createErr(String errorMessage) {
+        return new ErrorOr<>(errorMessage, null);
+    }
+
+    public boolean isError() {
+        return isErr;
+    }
+
+    public T get() {
+        return obj;
+    }
+
+    public String getError() {
+        return err;
+    }
 }
